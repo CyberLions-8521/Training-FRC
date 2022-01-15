4d5f0204fdef93a21a012e0b20ab4c1689fe58e6
Sacred <67220758+testmaster19@users.noreply.github.com>
2022-01-15 15:34:52 -0800

    commit

--- practice.java
+++ practice.java
@@ -3,6 +3,13 @@ class practice {
     public static void main(String []args) {
         System.out.println("hello world");
 
+        //adding coments
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter somethign cool");
+        String a = scan.nextLine();
+
+        System.out.println(a);
+
 
     }
    
