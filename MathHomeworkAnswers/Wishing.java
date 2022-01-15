090766ef143f05f9b65636c7c7abb4a03196d114
Sacred <67220758+testmaster19@users.noreply.github.com>
2022-01-15 15:41:40 -0800

    Added another folder

---
+++ MathHomeworkAnswers/Wishing.java
@@ -0,0 +1,8 @@
+package MathHomeworkAnswers;
+
+public class Wishing {
+    public static void main(String [] args) {
+        System.out.println("Okay");
+    }
+    
+}
---
+++ MathHomeworkAnswers/practice.java
@@ -0,0 +1,19 @@
+package MathHomeworkAnswers;
+
+class practice {
+    public static void main(String []args) {
+        System.out.println("hello world");
+
+        //adding coments
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter somethign cool");
+        String a = scan.nextLine();
+
+        System.out.println(a);
+        //commenting
+
+
+
+    }
+   
+}
