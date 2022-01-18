671163e8a6cc8fd78f2c9f81ee27e366c4d3654d
xaundyu <xaundiu@example.com>
2022-01-17 21:14:03 -0800

    finish HW

---
+++ DuyNguyen/game.java
@@ -0,0 +1,8 @@
+public class game {
+    public static void main(String [] args) {
+        System.out.println("My Favorite Game is red sus");
+        for (int i = 1; i > 0; i++){
+            System.out.println("Just Monika!");
+        }
+    }
+}
