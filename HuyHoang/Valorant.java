515c79c4f20383d148ef36962859f907f1cbebab
EV3KevinDEV <kevinholding92@gmail.com>
2022-01-17 21:30:41 -0800

    yo

--- HuyHoang/Valorant.java
+++
@@ -1,6 +0,0 @@
-public class Valorant {
-    public static void main(String [] args) {
-        System.out.println("My Favorite Game is Valorant.");
-    }
-    //I suck at Valorant, peaked silver 3 ;-;
-}
