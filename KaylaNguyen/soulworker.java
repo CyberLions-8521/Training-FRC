01ae80e6d819c324d457fc3ab3075512e217441f
Kavieko <knguyen0927@Gmail.com>
2022-01-17 21:01:19 -0800

    homework

---
+++ KaylaNguyen/soulworker.java
@@ -0,0 +1,7 @@
+package KaylaNguyen;
+public class soulworker {
+    public static void main(String [] args) {
+        System.out.println("My Favorite Game is soulworker.");
+    }
+    //im sleepy
+}
